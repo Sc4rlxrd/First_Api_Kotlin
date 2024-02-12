35a9b31bd0e4e0448afcd5e442dad3d2b202a887
Guilherme Santos <guilhermesantos2004gss@gmail.com>
2024-02-12 17:35:21 -0300

    Feat: init commit

---
+++ src/main/kotlin/com/scarlxrd/demoPerson/DemoPersonApplication.kt
@@ -0,0 +1,11 @@
+package com.scarlxrd.demoPerson
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoPersonApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoPersonApplication>(*args)
+}
